01dd43577a4934c16ddedd3d157a7903b9e7e72b
thong <2446871238@qq.com>
2020-07-05 22:46:47 +0800

    v1

--- v1/js/v1.js
+++ v1/js/v1.js
@@ -1,11 +1,21 @@
-var app1 = new Vue({
-	el:"#app-1",
-	data:{
-		message:"hello!!",
-		info:"tanhong",
-		list:['haiwang','haizai','xingji','tanhong'],
-		counter:0
-	},
+// ES6里面一般使用let(变量)和const(常量),不用var
+const obj ={
+	message:"hello!!",
+	info:"tanhong",
+	list:['haiwang','haizai','xingji','tanhong'],
+	counter:0,
+}
+
+
+const app1 = new Vue({
+	el:"#app-1",//用于挂载要管理的元素
+	// data:{  //定义数据,类型:object|function组件中data必须是一个函数）
+	// 	message:"hello!!",
+	// 	info:"tanhong",
+	// 	list:['haiwang','haizai','xingji','tanhong'],
+	// 	counter:0
+	// },
+	data: obj,
 	methods:{
 		add:function(){
 			var that = this;

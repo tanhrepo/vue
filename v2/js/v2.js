b12a41d6723898fa2478f0acecc151dcbfab7c86
thong <2446871238@qq.com>
2020-07-06 15:35:47 +0800

    v2

--- v2/js/v2.js
+++ v2/js/v2.js
@@ -1,7 +1,23 @@
-const app = new Vue({
-	el:"#app",
+const app1 = new Vue({
+	el:"#app1",
 	data:{
 		message:'hello',
+		info:'kobe',
+		lastname:'bryant',
+		counter:100
+	},
+	methods:{
+		created:function(){
+			console.log('created')
+		},
+	}
+})
+
+const app2 = new Vue({
+	el:"#app2",
+	data:{
+		message:'hello',
+		url:'<a href="https://www.baidu.com">百度一下</a>'
 	},
 	methods:{
 		created:function(){

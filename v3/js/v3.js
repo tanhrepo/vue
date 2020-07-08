0a1a8d93059e1744f4c09b8268931b6fe118d4cb
thong <2446871238@qq.com>
2020-07-08 21:35:15 +0800

    v3

--- v3/js/v3.js
+++ v3/js/v3.js
@@ -2,11 +2,12 @@ const app1 = new Vue({
 	el:"#app1",
 	data:{
 		message:'hello',
-		imgUrl:'https://img30.360buyimg.com/pop/s590x470_jfs/t1/143291/38/1926/45199/5efd953dEa9dda02e/7b68b2bc46cc9fe1.jpg.webp'
+		imgUrl:'http://p3.pstatp.com/large/ced0008cd353a7157a0',
+		aHref:'http://www.baidu.com',
 	},
 	methods:{
 		created:function(){
-			console.log('created')
+			console.log('created');
 		},
 	}
 })

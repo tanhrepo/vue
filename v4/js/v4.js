803d474535946d6d6d6134f4a87f061cbcf51794
thong <2446871238@qq.com>
2020-07-10 00:37:26 +0800

    v4

--- v4/js/v4.js
+++ v4/js/v4.js
@@ -13,3 +13,20 @@ const app1 = new Vue({
 		}
 	}
 })
+
+const app2 = new Vue({
+	el:"#app2",
+	data:{
+		// active:'active',
+		message:'数组',
+		baseStyle1:{backgroundColor:'red'},
+		baseStyle2:{fontSize:'40px'},
+		fsize1:'30px',
+		fcolor:'blue',
+	},
+	methods:{
+		getStyles:function(){
+			return{fontSize:this.fsize2+'px',color:this.fcolor}
+		}
+	}
+})

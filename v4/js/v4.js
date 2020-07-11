616bbeb3ca7085eebaefd73147bd8245855f72df
thong <2446871238@qq.com>
2020-07-11 14:03:57 +0800

    v4

--- v4/js/v4.js
+++ v4/js/v4.js
@@ -36,11 +36,31 @@ const app3 = new Vue({
 	data:{
 		firstName:'Jake',
 		lastName:'Rose',
+		books:[
+			{id:001,name:'代码大全',price:119},
+			{id:002,name:'计算机原理',price:79},
+			{id:003,name:'现代操作系统',price:159},
+			{id:004,name:'路由交换技术',price:80},
+		],
 	},
 	// computed:计算属性
 	computed:{
 		fullName:function(){
 			return this.firstName + ' ' + this.lastName;
+		},
+		totalPrice:function(){
+			// 方法一：
+			// let result = 0
+			// for(let i=0;i<this.books.length;i++){
+			// 	result += this.books[i].price
+			// }
+			// return result
+			// 方法二：
+			let result = 0
+			for(let i in this.books){
+				result += this.books[i].price
+			}
+			return result
 		}
 	},
 	methods:{

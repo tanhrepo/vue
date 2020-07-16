2cadec020dcd7cdf74d89f55e1f6a5ad25c51295
thong <2446871238@qq.com>
2020-07-16 15:44:18 +0800

    v9.js

--- v9/js/v9.js
+++ v9/js/v9.js
@@ -1,4 +1,36 @@
 const app1 = new Vue({
-	el:'#app1',
-	
+    el:'#app1',
+    data:{
+        books:[
+            {
+                id:1,
+                name:'算法',
+                data: '2016.01',
+                price:85,
+                count:1,
+            },
+            {
+                id:2,
+                name:'数据结构',
+                data: '2017.03',
+                price:45,
+                count:1,
+            },
+            {
+                id:3,
+                name:'天下',
+                data: '2017.08',
+                price:92,
+                count:1,
+            },
+            {
+                id:4,
+                name:'烽烟起',
+                data: '2016.01',
+                price:95,
+                count:1,
+            },
+
+        ]
+    }
 })

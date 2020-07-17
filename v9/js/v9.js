cd94288c18212abb5f1c11e5f3fca4e61d8cb888
thong <2446871238@qq.com>
2020-07-17 11:27:43 +0800

    v9,购物车小项目，编程范式

--- v9/js/v9.js
+++ v9/js/v9.js
@@ -69,3 +69,66 @@ const app1 = new Vue({
         }
     }
 })
+
+
+// 编程范式:命令式编程/声明式编程
+// 编程范式:面向对象编程(第一公民：对象)/面向函数编程(第一公民:函数)
+// filter/map/reduce
+// filter中的回调函数有一个要求：必须返回一个boolean值
+// true：当返回true的时候，函数内部会自动将这次回调的n加入到新的数组里面
+// false：当返回的是false时，函数内部会过滤掉这次的n
+
+const nums = [10,33,23,55,222,11,235,232]
+
+let total = nums.filter(n => n<100).map(n => n * 2).reduce((pre,n) => pre+n );
+console.log(total);
+
+
+// let total = nums.filter(function (n) {
+//     return n < 100
+// }).map(function (n) {
+//     return n * 2
+// }).reduce(function (preValue,n) {
+//     return preValue + n
+// },0)
+// console.log(total);
+
+// // 1.filter函数的使用
+// let newNums = nums.filter(function (n) {
+//     return n < 100
+// })
+// console.log(newNums);
+//
+// // 2.map函数的使用
+// let newNums2 = newNums.map(function (n) {
+//     return n * 2
+// })
+// console.log(newNums2);
+//
+// // 3.reduce函数的使用
+// let total = newNums2.reduce(function (preValue,n) {
+//     return preValue + n
+// },0)
+// console.log(total);
+
+
+// 1.需求:取出所有小于100的数
+// let newNums = []
+// for(let i of nums){
+//     if (i < 100){
+//         newNums.push(i)
+//     }
+// }
+//
+// // 2.需求：将取出的数都变大2倍
+// let newNums2 = []
+// for(let i of newNums){
+//     newNums2.push(i * 2)
+// }
+//
+// // 3.需求：将所有newNums2的数字相加，得到最终的结果
+// let total = 0
+// for (let n of newNums2){
+//     total += n
+// }
+// console.log(total);

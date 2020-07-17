ee93383d58079d711b2e09b71dc4589b3c52213f
thong <2446871238@qq.com>
2020-07-17 10:08:03 +0800

    v9，购物车小案例

--- v9/js/v9.js
+++ v9/js/v9.js
@@ -30,7 +30,42 @@ const app1 = new Vue({
                 price:95,
                 count:1,
             },
+            {
+                id:5,
+                name:'花火',
+                data: '2019.01',
+                price:66,
+                count:1,
+            },
+        ],
+    },
+    filters:{
+        showPrice(price){
+            return '¥' + price.toFixed(2)
+        }
+    },
+    methods:{
+        increment(index){
+            console.log('increment',index);
+            this.books[index].count++;
 
-        ]
+        },
+        decrement(index){
+            console.log('decrement',index);
+            this.books[index].count--;
+        },
+        removeHandle(index){
+            console.log('移除',index);
+            this.books.splice(index,1);
+        }
+    },
+    computed:{
+        totalPrice(){
+            let totalPrice = 0
+            for (let i = 0;i<this.books.length;i++){
+                totalPrice += this.books[i].price * this.books[i].count
+            }
+            return totalPrice
+        }
     }
 })

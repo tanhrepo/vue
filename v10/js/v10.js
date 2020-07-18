2d2a15bc6ac478b9b78c2ef0607287c15d8be85f
tanhong <2446871238@qq.com>
2020-07-18 22:06:52 +0800

    v10，echarts的实例

---
+++ v10/js/v10.js
@@ -0,0 +1,103 @@
+// 基于准备好的dom，初始化echarts实例
+const myChart = echarts.init(document.getElementById('main'));
+// 指定图表的配置项和数据
+option = {
+	backgroundColor: "#15251b",
+	title: {
+		text: '培训对象区域分布',
+		textStyle: {
+			color: '#0b8f5d',
+			fontSize: 20
+		}
+	},
+	tooltip: {
+		trigger: 'item',
+		formatter: '{a} <br/>{b}: {c} ({d}%)'
+	},
+	legend: {
+		icon: 'rect',
+		orient: 'horizontal', //设置为横向排列
+		itemWidth: 10,
+		itemHeight: 10,
+		borderRadius: 0, //设置了没有效果
+		bottom: 10,
+		data: ['涪陵', '南川', '丰都', '酉阳', '其它'],
+		textStyle:{
+			color: '#fff'
+		},
+	},
+	series: [{
+		name: '培训对象区域分布',
+		type: 'pie',
+		radius: ['50%', '70%'],
+		avoidLabelOverlap: false,
+		hoverAnimation: false,
+		startAngle: -70,
+		label: {
+			normal: {
+				formatter: '{c}%',
+				textStyle:{
+					color: '#fff'
+				},
+				rich: {
+					value: {
+						fontSize: 18,
+						fontWeight: 'bold',
+						color: '#fff'
+					}
+				},
+			}
+		},
+		emphasis: {
+			label: {
+				show: true,
+				// fontSize: '30',
+				// fontWeight: 'bold'
+			}
+		},
+		labelLine: {
+			show: false
+		},
+		data: [{
+				value: 27,
+				name: '涪陵',
+				itemStyle: {
+					color: '#00875e'
+				}
+			},
+			{
+				value: 16,
+				name: '南川',
+				itemStyle: {
+					color: '#53b1d3'
+				}
+			},
+			{
+				value: 33,
+				name: '丰都',
+				itemStyle: {
+					color: '#0073b6'
+				}
+			},
+			{
+				value: 24,
+				name: '酉阳',
+				itemStyle: {
+					color: '#7ad2a0'
+				}
+			},
+			{
+				value: 0,
+				name: '其它',
+				itemStyle: {
+					color: '#5c5cd6'
+				}
+			}
+			]
+	}]
+
+};
+
+
+// 使用刚指定的配置项和数据显示图表。
+myChart.setOption(option);

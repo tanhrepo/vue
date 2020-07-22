92f3b64bd944706ded5c9332bfbc3a459317546b
tanhong <2446871238@qq.com>
2020-07-22 22:02:01 +0800

    ；。

---
+++ v12/HotProduct.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="l-model-t">
+        <p class="p-title">扶贫热销产品排行</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HotProduct"
+    }
+</script>
+
+<style scoped>
+
+</style>

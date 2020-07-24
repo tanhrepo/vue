1c990ff058a7cb28ec1d27bb426bd8d96d674f17
tanhong <2446871238@qq.com>
2020-07-24 20:08:04 +0800

    。

---
+++ v12/CommunityDistribution.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="l-model-m">
+        <p class="p-title">益农社区分布情况</p>
+        <div class="c-table">
+            <table class="">
+                <thead>
+                <tr>
+                    <th>区域</th>
+                    <th>标准站</th>
+                    <th>简易站</th>
+                    <th>专业站</th>
+                    <th>中心站</th>
+                    <th>合计</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>渝中区</td>
+                    <td>20</td>
+                    <td>50</td>
+                    <td>20</td>
+                    <td>12</td>
+                    <td>141</td>
+                </tr>
+                <tr>
+                    <td>渝北区</td>
+                    <td>20</td>
+                    <td>50</td>
+                    <td>20</td>
+                    <td>12</td>
+                    <td>141</td>
+                </tr>
+                <tr>
+                    <td>云阳区</td>
+                    <td>20</td>
+                    <td>50</td>
+                    <td>20</td>
+                    <td>12</td>
+                    <td>141</td>
+                </tr>
+                <tr>
+                    <td>九龙坡区</td>
+                    <td>20</td>
+                    <td>50</td>
+                    <td>20</td>
+                    <td>12</td>
+                    <td>141</td>
+                </tr>
+                <tr>
+                    <td>沙坪坝区</td>
+                    <td>20</td>
+                    <td>50</td>
+                    <td>20</td>
+                    <td>12</td>
+                    <td>141</td>
+                </tr>
+                <tr>
+                    <td>沙坪坝区</td>
+                    <td>20</td>
+                    <td>50</td>
+                    <td>20</td>
+                    <td>12</td>
+                    <td>141</td>
+                </tr>
+                <tr>
+                    <td>沙坪坝区</td>
+                    <td>20</td>
+                    <td>50</td>
+                    <td>20</td>
+                    <td>12</td>
+                    <td>141</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "CommunityDistribution"
+    }
+</script>
+
+<style scoped>
+
+</style>

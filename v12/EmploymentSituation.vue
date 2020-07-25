0fb82da64db0451b2cd3f0e70f2b69c01140564d
tanhong <2446871238@qq.com>
2020-07-25 20:36:10 +0800

    xxxx

---
+++ v12/EmploymentSituation.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="l-model-t">
+        <p class="p-title">就业扶贫情况</p>
+        <div class="p-table">
+            <table cellspacing="10" class="" >
+                <tbody>
+                <tr>
+                    <td>岗位推荐数 {{num1}}</td>
+                    <td>意见就业数 {{num2}}</td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "EmploymentSituation",
+        data(){
+             return{
+                 num1:"1000",
+                 num2:"2222",
+             }
+        },
+    }
+</script>
+
+<style scoped>
+    .p-table{
+        width: 100%;
+        height: 19vh;
+        overflow: auto;
+        font-size: .8rem;
+        color: #afb8b3;
+        line-height: 2rem;
+        display: block;
+        /*margin-left: .7rem;*/
+        text-align: center;
+    }
+    .p-table table{
+        width: 100%;
+        border-collapse:separate;
+    }
+    .p-table tr{
+        overflow:hidden;
+        white-space:nowrap;
+    }
+    .p-table table tr td{
+        padding: .3rem;
+        background: #273a2f;
+    }
+
+</style>

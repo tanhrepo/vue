502b6cdb34e0b0a2f2d16856b85600fd96a35e4d
tanhong <2446871238@qq.com>
2020-08-03 21:50:36 +0800

    。。

--- v13/v13.js
+++ v13/v13.js
@@ -1,3 +1,28 @@
 window.onload = function(){
-	
+	//获取事件
+	let add = document.getElementById("add");
+	let sub = document.getElementById("sub");
+	let mul = document.getElementById("mul");
+	let divide = document.getElementById("divide");
+	//绑定事件
+	add.onclick = function(){
+		let num1 = window.parseFloat(document.getElementById("num1").value);
+		let num2 = window.parseFloat(document.getElementById("num2").value);
+		result.value = num1 + num2;
+	}
+	sub.onclick = function(){
+		let num1 = document.getElementById("num1").value;
+		let num2 = document.getElementById("num2").value;
+		result.value = num1 - num2
+	}
+	mul.onclick = function(){
+		let num1 = document.getElementById("num1").value;
+		let num2 = document.getElementById("num2").value;
+		result.value = num1 * num2
+	}
+	divide.onclick = function(){
+		let num1 = document.getElementById("num1").value;
+		let num2 = document.getElementById("num2").value;
+		result.value = num1 / num2
+	}
 }	

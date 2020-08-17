5830389794e36dc641b9e39f2210ed1b594c7e3b
tanhong <2446871238@qq.com>
2020-08-17 10:16:27 +0800

    17

---
+++ v17/v17.js
@@ -0,0 +1,12 @@
+import Spreadsheet from "x-data-spreadsheet";
+// If you need to override the default options, you can set the override
+// const options = {};
+// new Spreadsheet('#x-spreadsheet-demo', options);
+const s = new Spreadsheet("#x-spreadsheet-demo")
+  .loadData({}) // load data
+  .change(data => {
+    // save data to db
+  });
+
+// data validation
+s.validate()

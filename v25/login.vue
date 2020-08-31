40b0b5757fe3efab1641b736bd4a489514df9c74
tanhong <2446871238@qq.com>
2020-08-31 17:33:36 +0800

    。

---
+++ v25/login.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div id="content-left"></div>
+        <div id="content-right"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+    #content-left{
+        width: 81.5vw;
+        height: 50vh;
+        background-color: lightcoral;
+    }
+    #content-right{
+        width: 18.5vw;
+        height: 50vh;
+        background-color: lightblue;
+    }
+</style>

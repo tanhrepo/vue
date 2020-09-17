8d7df0fcf7c9601e2219f2315b8aa628c54797e2
tanhong <2446871238@qq.com>
2020-09-17 16:16:50 +0800

    canvas.js

--- canvas1/canvas.js
+++ canvas1/canvas.js
@@ -4,11 +4,21 @@ window.onload=function(){
 	canvas.height = 800;
 	
 	let context = canvas.getContext('2d');
-		context.moveTo(100,100);
-		context.lineTo(700,700);
-		context.lineTo(100,700);
-		context.lineTo(100,100);
-		context.lineWidth = 3;
-		context.strokeStyle = "#058"
-		context.stroke()
+	context.lineWidth = 3;
+	
+	context.beginPath();
+	context.moveTo(100,100);
+	context.lineTo(700,700);
+	context.strokeStyle = "red"
+	context.stroke()
+	context.beginPath();
+	context.moveTo(700,700);
+	context.lineTo(100,700);
+	context.strokeStyle = "lightblue"
+	context.stroke()
+	context.beginPath();
+	context.moveTo(100,700);
+	context.lineTo(100,100);
+	context.strokeStyle = "#058"
+	context.stroke()
 }

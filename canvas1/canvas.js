aecfabb9df4dd1de65cb9cf2e143f648dadf2fa8
tanhong <2446871238@qq.com>
2020-09-17 14:52:36 +0800

    111

--- canvas1/canvas.js
+++ canvas1/canvas.js
@@ -6,6 +6,9 @@ window.onload=function(){
 	let context = canvas.getContext('2d');
 		context.moveTo(100,100);
 		context.lineTo(700,700);
+		context.lineTo(100,700);
+		context.lineTo(100,100);
 		context.lineWidth = 3;
+		context.strokeStyle = "#058"
 		context.stroke()
 }

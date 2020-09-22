0c36fb95c107a07a58f8e505edf02ebbb9fcdd2f
tanhong <2446871238@qq.com>
2020-09-22 14:48:18 +0800

    原型

--- yuanxing/proto.js
+++ yuanxing/proto.js
@@ -1,8 +1,6 @@
 // 函数也是对象
 function User(){};
 
-// console.dir()可以显示一个对象的所有属性和方法
-
 User.__proto__.view = function(){
 	console.log("user 的__proto__")
 }
@@ -16,5 +14,20 @@ let hd = new User();
 hd.show()
 console.log(User.prototype==hd.__proto__)
 
+// console.dir()可以显示一个对象的所有属性和方法
 console.dir(User);
 console.dir(hd);
+
+// String,Number,Object
+Object.prototype.zeng = function(){
+	console.log("这里是object.portotype")
+}
+
+let hdr = new Object();
+hdr.name = "tianxia";
+console.dir(Object.prototype);
+console.log(User.prototype.__proto__ == User.__proto__.__proto__);
+
+let th = new User();
+th.show();
+th.zeng();

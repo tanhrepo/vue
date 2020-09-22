e68d10616fc782f828a9adf98f7c3113aa7475da
tanhong <2446871238@qq.com>
2020-09-22 17:33:27 +0800

    原型2

--- yuanxing2/yuanxing.js
+++ yuanxing2/yuanxing.js
@@ -14,3 +14,30 @@ parent.show()
 
 console.log(Object.getPrototypeOf(hd))
 
+function User(name){
+	this.name = name;
+}
+
+console.dir(User)
+console.log(User.prototype.__proto__ == Object.prototype)//6
+console.log(Object.prototype.constructor.constructor == User)//7
+console.log(User.prototype.constructor == User)//8
+
+// User.prototype.show = function(){//1. 在对象里面追加一个show属性
+// 	console.log(this.name)
+// }
+
+User.prototype = {//2. 覆盖掉原来的
+	constructor:User,//要将原来的属性添加上
+	show(){
+		console.log(this.name)
+	}
+}
+
+let zhangsan = new User.prototype.constructor("张三")
+let lisi = new User("李四")
+
+console.log(zhangsan)
+console.log(lisi)
+lisi.show()
+zhangsan.show()

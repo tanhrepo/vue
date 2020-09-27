4ad22e21ce001c7b4938ea38a12873308d570fde
tanhong <2446871238@qq.com>
2020-09-27 16:22:44 +0800

    原型2， constructor没有看懂，

--- yuanxing2/yuanxing.js
+++ yuanxing2/yuanxing.js
@@ -16,7 +16,17 @@ console.log(Object.getPrototypeOf(hd))
 
 function User(name){
 	this.name = name;
+	this.show = function(){
+		console.log(this.name)
+	}
 }
+
+// 原型就是一个对象,只要是对象,那么他也有自己的原型
+// prototype和__ptoto__都可以理解为原型,
+// 把构造函数作为对象调用的时候我们可以用下面__proto__,他服务于他自己,
+// 另外一个是我们通过构造函数new出来无数个对象的时候,
+// 这时候无数个对象就使用prototype,
+// 相比来说prototype的威力更强一点,因为它可以作用于无数个对象
 
 console.dir(User)
 console.log(User.prototype.__proto__ == Object.prototype)//6
@@ -44,6 +54,12 @@ zhangsan.show()
 
 
 function createByobject(obj,...args){
-	
+	const constructor = Object.getPrototypeOf(obj).constructor;
+	console.log(constructor);
+	console.log(constructor == User);//true
+	return new constructor(...args);
 }
-let th = createByobject(th,'tanhong')
+
+
+let th = createByobject(lisi,'tanhong')
+th.show();

6837be481337417a4cc0c74b096786bcaeeedaa8
tanhong <2446871238@qq.com>
2020-09-29 11:48:33 +0800

    笔记

--- js笔记/new_file.js
+++ js笔记/new_file.js
@@ -1,8 +1,7 @@
 function Foo() {
-    var i = 2;
+    var i = 0;
     return function() {
         console.log(i++);
-		console.log(i);
     }
 }
  
@@ -10,15 +9,15 @@ var f1 = Foo(),
     f2 = Foo();
 console.dir(f1.__proto__() == Foo.__proto__())
 console.dir(f1 == f2)
-// f1();
+f1();
 // f1();
 // f1();
 // 0 9 8 8 01
-let a = 0
-let b = 0
+// let a = 0
+// let b = 0
 // console.log(a=a++)
-a = a++
-console.log(a)
-b = a++
-console.log(a)
-console.log(b)
+// a = a++
+// console.log(a)
+// b = a++
+// console.log(a)
+// console.log(b)

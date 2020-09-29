f3f80e32113f0cb262880e206ae2b4cad7268b65
tanhong <2446871238@qq.com>
2020-09-29 10:56:55 +0800

    错题笔记

---
+++ js笔记/new_file.js
@@ -0,0 +1,24 @@
+function Foo() {
+    var i = 2;
+    return function() {
+        console.log(i++);
+		console.log(i);
+    }
+}
+ 
+var f1 = Foo(),
+    f2 = Foo();
+console.dir(f1.__proto__() == Foo.__proto__())
+console.dir(f1 == f2)
+// f1();
+// f1();
+// f1();
+// 0 9 8 9 01
+let a = 0
+let b = 0
+// console.log(a=a++)
+a = a++
+console.log(a)
+b = a++
+console.log(a)
+console.log(b)

dce6a74631b116c820075f5bdd7ffb643945cb8e
tanhong <2446871238@qq.com>
2020-10-14 23:13:23 +0800

    1

--- async/await/index.js
+++ async/await/index.js
@@ -142,18 +142,29 @@ function axios(){
 // 箭头函数、async 、await
 // async,await最简单的使用 可以省略掉.then 简单快捷
 document.getElementById("btn").onclick = async()=>{
-	let res = await axios();//这里会等待成功，再执行下面的
+	let res1 = await axios();//这里会等待成功，再执行下面的
+	let res2 = await axios();//这里会等待成功，再执行下面的
 	
-	console.log("2点击发送ajax",res);
+	console.log("2点击发送ajax",res1,res2);
 }
 
 // async函数就是Generator函数的语法糖	
 // Generator是ES6提出的异步编程解决方案，语法行为与传统函数完全不同
 // 形式上，Generator函数是一个普通函数，
 // 但是有两个特征，一是function关键字与函数名之间有一个星号；二是函数体内部使用yield表达式，
-// Generator
-function* helloworldGenerator(){
-	yield 'hello';
-	yield 'world';
-	return 'ending';
-}
+// Generator 里面的代码是 分段执行 遇到yield就分一段
+// function* helloworldGenerator(){
+// 	yield 'hello';//yield 相当于一个暂停标记
+// 	yield 'world';
+// 	return 'ending';
+// }
+
+// var hw = helloworldGenerator();//hw返回一个指针
+// console.log(hw)//这个函数的结果是ending吗？ 不是 因为代码是暂停的 得到的是一个暂停标记，指向hello
+
+// console.log(hw.next())//指向hello的下一步就得到hello，next才能拿出这个暂停的值
+// console.log(hw.next())//再下一步就拿到world
+// console.log(hw.next())
+// console.log(hw.next())//没有了就返回undefined
+
+// async + await 原理就是Generator + yield

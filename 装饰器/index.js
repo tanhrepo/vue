68146e53f26950a13456bf264b9723f8519b29b3
tanhong <2446871238@qq.com>
2020-10-29 09:09:52 +0800

    去掉逗号

--- 装饰器/index.js
+++ 装饰器/index.js
@@ -17,7 +17,7 @@ D:\home\WebStorm\cqoa\ruoyi-quartz\target\classes
 D:\home\WebStorm\cqoa\ruoyi-quartz\target\test-classes
 
 D:\home\WebStorm\cqoa\ruoyi-system\target\classes
-D:\home\WebStorm\cqoa\ruoyi-system\target\test-classes,
+D:\home\WebStorm\cqoa\ruoyi-system\target\test-classes
 //若依输出路径？？？
 // 装饰器(Decorator)是一种与类(class)相关的语法,用来注释修改类和类方法与属性.许多面向对象的语言都有这一项功能.一般和类class相关,普通函数不要使用
 

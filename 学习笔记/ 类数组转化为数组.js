12e95a660d461d1d663086bc4d2300a5b72dcd09
tanhong <tanhong@asiainfo.com>
2020-11-20 10:38:59 +0800

    数组

--- 学习笔记/ 类数组转化为数组.js
+++ 学习笔记/ 类数组转化为数组.js
@@ -1,1 +1,4 @@
 Array.from(document.querySelectorAll('div'))
+Array.prototype.slice.call(document.querySelectorAll('div'));
+[...document.querySelectorAll('div')]
+Array.prototype.concat.apply([],document.querySelectorAll('div'))

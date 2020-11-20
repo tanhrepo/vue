f755231b7ef326f1b41c17278f1560ce9760154a
tanhong <tanhong@asiainfo.com>
2020-11-20 16:26:00 +0800

    队列

---
+++ js/队列.js
@@ -0,0 +1,61 @@
+// 队列的封装、
+// 基于数组
+function Queue(val){
+	this.items = []
+	
+	// 方法
+	// 1.将元素加入队列
+	Queue.prototype.enqueue = function(enNum){
+		this.items.push(enNum)
+	}
+	// 2.从队列中删除元素
+	Queue.prototype.dequeue = function(){
+		this.items.shift()
+	}
+	// 3.查看前端的元素
+	Queue.prototype.front = function(){
+		return this.items[0]
+	}
+	// 4.查看队列是否为空
+	Queue.prototype.isEmpty = function(){
+		return this.items.length === 0
+	}
+	// 5.查看队列中元素的个数
+	Queue.prototype.size = function(){
+		return this.items.length
+	}
+	// 6.toString方法
+	Queue.prototype.toString = function(){
+		// 和栈类似
+		let resultString = '';
+		for(let i = 0,j = this.items.length;i < j;i++){
+			resultString += this.items[i] + ' '
+		}
+		return resultString
+	}
+}
+
+// 使用队列
+let queue = new Queue()
+// 将元素加入队列
+queue.enqueue(123);
+queue.enqueue('abc');
+queue.enqueue(34);
+queue.enqueue(67);
+console.log(queue)
+// 删除队列前端元素
+queue.dequeue()
+console.log(queue)
+console.log(queue.front())
+console.log(queue.isEmpty())
+console.log(queue.size())
+console.log(queue.toString())
+
+// 击鼓传花
+// 我们来修改一下这个游戏规则.
+// 几个朋友一起玩一个游戏,围成一圈,开始数数,数到某个数字的人自动淘汰。
+// 最后剩下的这个人会获得胜利,请问最后剩下的是原来在哪一个位置上的人?
+
+// 封装一个基于队列的函数
+// 参数：所有参与人的名字，基于的数字
+// 结果：最终剩下一个人的名字

6113df4faeac3123b4b0427454e8e42363f8f923
tanhong <tanhong@asiainfo.com>
2020-11-24 18:48:24 +0800

    axios

--- axios/index.js
+++ axios/index.js
@@ -1,1 +1,22 @@
-// mvvm
+// axios
+// 功能特点
+// 1. 在浏览器发送XMLHttpRequest请求
+// 2. 在node里面发送http请求
+// 3. 支持Promise API
+// 4. 拦截请求和响应
+// 5. 转换请求和响应数据
+// ...
+
+// axios(ajax I/O system)  hhh
+
+// axios的请求方式
+// axios(config)
+// axios.request(config)
+// axios.get(url[,config])
+// axios.delete(url[,config])
+// axios.head(url[,config])
+// axios.post(url[,data[,config]])
+// axios.put(url[,data[,config]])
+// axios.patch(url[,data[,config]])
+
+

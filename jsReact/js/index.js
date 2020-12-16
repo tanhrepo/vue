09ae696dc8fa24ca5377d2b97bbc21f650710b12
tanhong <tanhong@asiainfo.com>
2020-12-17 00:03:56 +0800

    js

--- jsReact/js/index.js
+++ jsReact/js/index.js
@@ -1,10 +1,37 @@
 // 一门编程语言,
 // 变量 函数 数据结构 运算能力
+
 // let a; //变量声明
 // a = 3; //变量赋值
 // let a = 3; //变量声明并赋值
 
-let x = 1,
-	y = 2; //换行对齐，最好强制加上分号
+// let x = 1,
+// 	y = 2; //换行对齐，最好强制加上分号
 
-document.write(x)
+// document.write(x)
+
+// 开发命名规范：
+// 	不能数字开头
+// 	能用_$开头
+// 	字母数字$_等
+// 	不要用关键字保留字https://www.w3school.com.cn/js/pro_js_keywords.asp
+//	语义化 结构化命名
+//	变量名用小驼峰(myEnglishName)
+
+// let x = 3,
+// 	y = 4;
+// 	z = x + y;	//先运算了再赋值
+// document.write(z)//7，
+//运算 > 赋值 优先级
+
+// 动态语言 -> 脚本语言 -> 解释性语言 -> 弱类型语言
+// 静态语言 -> 编译性语言 -> 强类型语言
+
+// JavaScript 数据类型
+
+// 原始值：基本类型：字符串（String）、数字(Number)、布尔(Boolean)、空（Null）、未定义（Undefined）。
+// 引用值：数组(Array)、对象(Object)、函数(function)、date(日期)、RegExp(正则)
+
+// let a;
+// document.write(a)//undefined
+
